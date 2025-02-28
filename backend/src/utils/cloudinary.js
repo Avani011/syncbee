cb25f83083e02dbd860a8000db7135fd8eb39e75
Avani Agarwal <102690647+Avani011@users.noreply.github.com>
2025-02-28 05:54:13 +0530

    Add Cloudinary integration and Multer middleware for file uploads

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+
+const uploadOnCloudinary = async (loclFilePath) => {
+    try {
+        if(!loclFilePath) return null
+
+        //upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+
+        //file has been uploaded successfully
+        console.log('File uploaded successfully', response.url);
+
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove locally saved temporary file 
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
